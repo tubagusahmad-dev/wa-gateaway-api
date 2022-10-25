1a8eb8f17dc75c8c205b4b877bbd2762c93740e1
tubagusahmad-dev <hi.tubagusahmad@gmail.com>
2022-10-25 10:11:49 -0400

    Push Again

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-const waConnect = require('./wa-connect.js');
+const waConnect = require('./lib/wa-connect.js');
 
 const APP_HOST = process.env.APP_HOST;
 const APP_PORT = parseInt(process.env.APP_PORT);
@@ -17,12 +17,20 @@ const APP_PORT = parseInt(process.env.APP_PORT);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/lib/js/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/lib/css/tubagus_css', express.static(__dirname + '/lib/css/tubagus_css'));
-app.use('/lib/js/feather', express.static(__dirname + '/lib/js/feather'));
-app.use('/file/image', express.static(__dirname + '/assets/images'));
+app.use('/lib/css/tubagus_css', express.static(__dirname + '/view/lib/css/tubagus_css'));
+app.use('/lib/js/feather', express.static(__dirname + '/view/lib/js/feather'));
+app.use('/file/image', express.static(__dirname + '/assets/image'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/view/index.html');
+});
+
+app.get('/devices', (req, res) => {
+    res.status(200).json(require('./lib/devices.js').get()).end();
+});
+
+app.get('/test', (req, res) => {
+    res.status(200).send(req.headers.host).end();
 });
 
 io.on('connection', (socket) => {
